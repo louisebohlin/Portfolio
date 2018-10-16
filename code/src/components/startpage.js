fd5a95060fc9278e1dcf77f90098b259b8fcd745
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-10-16 12:43:51 +0200

    Front page with buttons and some styling

--- code/src/components/startpage.js
+++ code/src/components/startpage.js
@@ -32,19 +32,27 @@ class Startpage extends React.Component {
             <p>louisebp@gmail.com +46707425010</p>
           </div>
           <div className="presentationtext">
+            <div>
             <h3>Louise Böhlin Pihlsgård</h3>
             <h1>Frontend developer + Graphic Designer</h1>
-            <p>Hello! I&#39;m an outgoing introvert who loves clean code, nice design and mind blowing technology. Diverse background in tech, humanities and design and dreaming about working with passionate people in a stimulating environment.</p>
+            </div>
+            <div>
+            <p>Hello! I&#39;m an outgoing introvert who loves clean code, nice design and mind blowing technology. I have a diverse background in tech, humanities and design and dream about working with passionate people in a stimulating environment.</p>
+            <a href="https://github.com/louisebohlin" target="_blank"><button>See my Github</button></a>
+            </div>
           </div>
         </div>
         <div className="tech-div">
           <h1>Tech</h1>
-          <h3>480 coding hours  ✨  50+ individual tasks ✨  HTML5, CSS, SASS, Flexbox, Javascript, ES6, JSX, React, Node.js, API:s ✨ 6 sprints with demos together with companies like Volumental, Bonnier, Comprend @ Technigo
+          <h3>480 coding hours  ✨  50+ individual tasks ✨  HTML5, CSS, SASS, Flexbox, Javascript, ES6, JSX, React, Node.js, API:s ✨ 6 sprints with demos together with companies like Volumental, Bonnier, Comprend, Svenska Spel @ Technigo
           </h3>
         </div>
         <Workexample />
         <Skills />
         <Studies />
+        <div className="linkedin">
+          <a href="https://www.linkedin.com/in/louise-b%C3%B6hlin-pihlsg%C3%A5rd-96687010b/" target="_blank"><button>See my Linkedin</button></a>
+        </div>
         <Footer />
       </div>
 
