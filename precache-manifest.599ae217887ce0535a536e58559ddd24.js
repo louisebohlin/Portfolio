1db953ea5a271e264f772772a52dbed05d39c2eb
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-10-16 15:03:31 +0200

    Updates

--- precache-manifest.599ae217887ce0535a536e58559ddd24.js
+++ precache-manifest.599ae217887ce0535a536e58559ddd24.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Portfolio/static/js/runtime~main.3b961160.js"
   },
   {
-    "revision": "89a6a6f7cea51d4fdc48",
-    "url": "/Portfolio/static/js/main.89a6a6f7.chunk.js"
+    "revision": "dca81e83ddb912ffa14d",
+    "url": "/Portfolio/static/js/main.dca81e83.chunk.js"
   },
   {
     "revision": "39a7ed922c4b697a51cb",
     "url": "/Portfolio/static/js/1.39a7ed92.chunk.js"
   },
   {
-    "revision": "89a6a6f7cea51d4fdc48",
+    "revision": "dca81e83ddb912ffa14d",
     "url": "/Portfolio/static/css/main.5540168c.chunk.css"
   },
   {
-    "revision": "cd2c62cce57469cfd9e85ebed6b9dec1",
+    "revision": "658443bfbeb8898bb65ff94a45851309",
     "url": "/Portfolio/index.html"
   }
 ];
