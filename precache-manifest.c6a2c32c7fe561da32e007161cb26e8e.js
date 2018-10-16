d495253c6cdc653f562848364b2d6d321645ce65
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-10-16 14:41:54 +0200

    Updates

---
+++ precache-manifest.c6a2c32c7fe561da32e007161cb26e8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "89a6a6f7cea51d4fdc48",
+    "url": "/Portfolio/static/js/main.89a6a6f7.chunk.js"
+  },
+  {
+    "revision": "39a7ed922c4b697a51cb",
+    "url": "/Portfolio/static/js/1.39a7ed92.chunk.js"
+  },
+  {
+    "revision": "89a6a6f7cea51d4fdc48",
+    "url": "/Portfolio/static/css/main.5540168c.chunk.css"
+  },
+  {
+    "revision": "cd2c62cce57469cfd9e85ebed6b9dec1",
+    "url": "/Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.0a787348995491932e19021ddfe55cbc.js"
+  "/Portfolio/precache-manifest.c6a2c32c7fe561da32e007161cb26e8e.js"
 );
 
 workbox.clientsClaim();
