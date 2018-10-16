f9cb52d2b3bf5a7b97b30fa73656c01f9b15b222
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2018-10-16 14:43:01 +0200

    Shaped up the json file

--- code/src/components/workexample/workexample.js
+++ code/src/components/workexample/workexample.js
@@ -8,27 +8,27 @@ class Workexample extends React.Component {
     return (
         <div className="workexample-container">
           <div className="workexample">
-            <h4>Flight form with CSS written with SASS.</h4>
+            <h4>Flight form with CSS written with SASS</h4>
             <Link to="/projectpage/1"><img src="./images/flightform.jpg" /></Link>
           </div>
           <div className="workexample">
-            <h4>Product listing page in React.</h4>
+            <h4>Product listing page in React</h4>
             <Link to="/projectpage/2"><img src="./images/productpage.png" /></Link>
           </div>
           <div className="workexample">
-            <h4>Signup form which posts the data into a HTTPbin.</h4>
+            <h4>Signup form which posts the data into a HTTPbin</h4>
             <Link to="/projectpage/3"><img src="./images/form.png" /></Link>
           </div>
           <div className="workexample">
-            <h4>Radio player built in React using Sveriges Radios API:s.</h4>
+            <h4>Radio player built in React using Sveriges Radios API:s</h4>
             <Link to="/projectpage/4"><img src="./images/radioplayer.png" /></Link>
           </div>
           <div className="workexample">
-            <h4>Radio player built in React using Sveriges Radios API:s.</h4>
+            <h4>VAT calculator built with controlled components in React</h4>
             <Link to="/projectpage/5"><img src="./images/vat.png" /></Link>
           </div>
           <div className="workexample">
-            <h4>Radio player built in React using Sveriges Radios API:s.</h4>
+            <h4>Snake Game in Unity</h4>
             <Link to="/projectpage/6"><img src="./images/snake.png" /></Link>
           </div>
         </div>
